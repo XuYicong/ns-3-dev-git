lanante/ns-3-dev-11ax

wifi: Add unique ID for PPDUs

src/wifi/model/wifi-phy.cc
@@ -51,6 +51,8 @@ NS_LOG_COMPONENT_DEFINE ("WifiPhy");
 
 NS_OBJECT_ENSURE_REGISTERED (WifiPhy);
 
+uint64_t WifiPhy::m_globalPpduUid = 0;
+
 /**
  * This table maintains the mapping of valid ChannelNumber to
  * Frequency/ChannelWidth pairs.  If you want to make a channel applicable
@@ -2636,7 +2638,7 @@ WifiPhy::Send (WifiPsduMap psdus, WifiTxVector txVector)
       return;
     }
 
-  Ptr<WifiPpdu> ppdu = Create<WifiPpdu> (psdus, txVector, txDuration, GetFrequency ());
+  Ptr<WifiPpdu> ppdu = Create<WifiPpdu> (psdus, txVector, txDuration, GetFrequency (), m_globalPpduUid++);
 
   if (m_wifiRadioEnergyModel != 0 && m_wifiRadioEnergyModel->GetMaximumTimeInState (WifiPhyState::TX) < txDuration)
     {

src/wifi/model/wifi-ppdu.cc
@@ -29,27 +29,31 @@ namespace ns3 {
 
 NS_LOG_COMPONENT_DEFINE ("WifiPpdu");
 
-WifiPpdu::WifiPpdu (Ptr<const WifiPsdu> psdu, WifiTxVector txVector, Time ppduDuration, uint16_t frequency)
+WifiPpdu::WifiPpdu (Ptr<const WifiPsdu> psdu, WifiTxVector txVector, Time ppduDuration, uint16_t frequency,
+                    uint64_t uid)
   : m_preamble (txVector.GetPreambleType ()),
     m_modulation (txVector.IsValid () ? txVector.GetMode ().GetModulationClass () : WIFI_MOD_CLASS_UNKNOWN),
     m_truncatedTx (false),
     m_frequency (frequency),
-    m_channelWidth (txVector.GetChannelWidth ())
+    m_channelWidth (txVector.GetChannelWidth ()),
+    m_uid (uid)
 {
-  NS_LOG_FUNCTION (this << *psdu << txVector << ppduDuration << frequency);
+  NS_LOG_FUNCTION (this << *psdu << txVector << ppduDuration << frequency << uid);
   m_psdus.insert (std::make_pair (SU_STA_ID, psdu));
   SetPhyHeaders (txVector, ppduDuration, frequency);
 }
 
-WifiPpdu::WifiPpdu (const WifiPsduMap & psdus, WifiTxVector txVector, Time ppduDuration, uint16_t frequency)
+WifiPpdu::WifiPpdu (const WifiPsduMap & psdus, WifiTxVector txVector, Time ppduDuration, uint16_t frequency,
+                    uint64_t uid)
   : m_preamble (txVector.GetPreambleType ()),
     m_modulation (txVector.IsValid () ? txVector.GetMode (psdus.begin()->first).GetModulationClass () : WIFI_MOD_CLASS_UNKNOWN),
     m_psdus (psdus),
     m_truncatedTx (false),
     m_frequency (frequency),
-    m_channelWidth (txVector.GetChannelWidth ())
+    m_channelWidth (txVector.GetChannelWidth ()),
+    m_uid (uid)
 {
-  NS_LOG_FUNCTION (this << psdus << txVector << ppduDuration << frequency);
+  NS_LOG_FUNCTION (this << psdus << txVector << ppduDuration << frequency << uid);
   if (m_preamble == WIFI_PREAMBLE_HE_MU || m_preamble == WIFI_PREAMBLE_HE_TB)
     {
       m_muUserInfos = txVector.GetHeMuUserInfoMap ();
@@ -651,12 +655,19 @@ WifiPpdu::GetModulation (void) const
   return m_modulation;
 }
 
+uint64_t
+WifiPpdu::GetUid (void) const
+{
+  return m_uid;
+}
+
 void
 WifiPpdu::Print (std::ostream& os) const
 {
   os << "preamble=" << m_preamble
      << ", modulation=" << m_modulation
-     << ", truncatedTx=" << (m_truncatedTx ? "Y" : "N");
+     << ", truncatedTx=" << (m_truncatedTx ? "Y" : "N")
+     << ", uid=" << m_uid;
   IsMu () ? (os << ", " << m_psdus) : (os << ", PSDU=" << m_psdus.at (SU_STA_ID));
 }
 

XuYicong/ns-3-dev-git

src/wifi/model/wifi-phy.cc
@@ -50,6 +50,8 @@ NS_LOG_COMPONENT_DEFINE ("WifiPhy");
 
 NS_OBJECT_ENSURE_REGISTERED (WifiPhy);
 
+uint64_t WifiPhy::m_globalPpduUid = 0;
+
 /**
  * This table maintains the mapping of valid ChannelNumber to
  * Frequency/ChannelWidth pairs.  If you want to make a channel applicable
@@ -2851,7 +2853,7 @@ WifiPhy::Send (WifiConstPsduMap psdus, WifiTxVector txVector)
     }
   m_state->SwitchToTx (txDuration, psdus, GetPowerDbm (txVector.GetTxPowerLevel ()), txVector);
 
-  Ptr<WifiPpdu> ppdu = Create<WifiPpdu> (psdus, txVector, txDuration, GetPhyBand ());
+  Ptr<WifiPpdu> ppdu = Create<WifiPpdu> (psdus, txVector, txDuration, GetPhyBand (), m_globalPpduUid++);
 
   if (m_wifiRadioEnergyModel != 0 && m_wifiRadioEnergyModel->GetMaximumTimeInState (WifiPhyState::TX) < txDuration)
     {

src/wifi/model/wifi-ppdu.cc
@@ -29,28 +29,30 @@ namespace ns3 {
 
 NS_LOG_COMPONENT_DEFINE ("WifiPpdu");
 
-WifiPpdu::WifiPpdu (Ptr<const WifiPsdu> psdu, WifiTxVector txVector, Time ppduDuration, WifiPhyBand band)
+WifiPpdu::WifiPpdu (Ptr<const WifiPsdu> psdu, WifiTxVector txVector, Time ppduDuration, WifiPhyBand band, uint64_t uid)
   : m_preamble (txVector.GetPreambleType ()),
     m_modulation (txVector.IsValid () ? txVector.GetMode ().GetModulationClass () : WIFI_MOD_CLASS_UNKNOWN),
     m_truncatedTx (false),
     m_band (band),
     m_channelWidth (txVector.GetChannelWidth ()),
-    m_txPowerLevel (txVector.GetTxPowerLevel ())
+    m_txPowerLevel (txVector.GetTxPowerLevel ()),
+    m_uid (uid)
 {
-  NS_LOG_FUNCTION (this << *psdu << txVector << ppduDuration << band);
+  NS_LOG_FUNCTION (this << *psdu << txVector << ppduDuration << band << uid);
   m_psdus.insert (std::make_pair (SU_STA_ID, psdu));
   SetPhyHeaders (txVector, ppduDuration, band);
 }
 
-WifiPpdu::WifiPpdu (const WifiConstPsduMap & psdus, WifiTxVector txVector, Time ppduDuration, WifiPhyBand band)
+WifiPpdu::WifiPpdu (const WifiConstPsduMap & psdus, WifiTxVector txVector, Time ppduDuration, WifiPhyBand band, uint64_t uid)
   : m_preamble (txVector.GetPreambleType ()),
     m_modulation (txVector.IsValid () ? txVector.GetMode (psdus.begin()->first).GetModulationClass () : WIFI_MOD_CLASS_UNKNOWN),
     m_psdus (psdus),
     m_truncatedTx (false),
     m_band (band),
-    m_channelWidth (txVector.GetChannelWidth ())
+    m_channelWidth (txVector.GetChannelWidth ()),
+    m_uid (uid)
 {
-  NS_LOG_FUNCTION (this << psdus << txVector << ppduDuration << band);
+  NS_LOG_FUNCTION (this << psdus << txVector << ppduDuration << band << uid);
   if (txVector.IsMu ())
     {
       m_muUserInfos = txVector.GetHeMuUserInfoMap ();
@@ -501,12 +503,19 @@ WifiPpdu::GetModulation (void) const
   return m_modulation;
 }
 
+uint64_t
+WifiPpdu::GetUid (void) const
+{
+  return m_uid;
+}
+
 void
 WifiPpdu::Print (std::ostream& os) const
 {
   os << "preamble=" << m_preamble
      << ", modulation=" << m_modulation
-     << ", truncatedTx=" << (m_truncatedTx ? "Y" : "N");
+     << ", truncatedTx=" << (m_truncatedTx ? "Y" : "N")
+     << ", uid=" << m_uid;
   IsMu () ? (os << ", " << m_psdus) : (os << ", PSDU=" << m_psdus.at (SU_STA_ID));
 }
 